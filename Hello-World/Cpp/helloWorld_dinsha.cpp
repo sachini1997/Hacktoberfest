0dbd0e8f36351c60fe3d89135c1e6af6751b2a9a
Dinsha <dinsha.jaiswal123@gmail.com>
2021-10-04 20:07:04 +0530

    Added a hello world

---
+++ Hello-World/Cpp/helloWorld_dinsha.cpp
@@ -0,0 +1,16 @@
+/*
+LANGUAGE: CPP
+AUTHOR: Dinsha Jaiswal
+GITHUB: https://github.com/fleurdelis07
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    cout<<"Hello, World!";
+    
+    return 0;
+}
